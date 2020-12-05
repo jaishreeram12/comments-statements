c92bc339567d4b83f773b059227a2d14e28a98b2
jaishreeram12 <75468475+jaishreeram12@users.noreply.github.com>
2020-12-05 12:35:28 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,10 @@
+var1="hello "
+var2="billy butcher"
+var3=3.8
+var4=8
+"""strings can be added with strings only
+and int can do any arthmetic operations"""
+print(var1+var2)
+print(var3*var4)
+print(type(var4))
+print(type(var3))
