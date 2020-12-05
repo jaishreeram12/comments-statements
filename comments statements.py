21d7d2411e76c90df01d78cc609f5d3caaac0994
jaishreeram12 <75468475+jaishreeram12@users.noreply.github.com>
2020-12-05 11:42:58 +0530

    Add files via upload

---
+++ comments statements.py
@@ -0,0 +1,6 @@
+#please dont remove this line
+"""this is a comment"""
+"""python is very easy language"""
+#print("subscribe code with vishal","there are 28 states in india",end="and")
+#print(" popat")
+print("c=\nsmakia\t1")
